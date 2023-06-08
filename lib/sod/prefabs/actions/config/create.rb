4dc65ae3c619970ffd5f1e0aa8d0521090e6837a
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-08 11:27:36 -0600

    Added prefabrication for configuration create action
    Necessary to provide a prefabricated action which can be used by upstream projects for creating a project's configuration.
    
    Use of `ARGV.clear` is necessary since `Kernel#gets` reads from it. So if anything, *dirty*, is in `ARGV` then it'll mess up how `Kernel#gets` processes user input. Even though this is a destructive mutation, it's OK in this situation since excecution of this action is the last thing that happens before the CLI exits. In the future, prompting for user input might need to be purely a CLI flag to avoid prompting altogether.
    
    💡 This action pairs well with the following gems:
    
    - [Etcher](https://alchemists.io/projects/etcher)
    - [Runcom](https://alchemists.io/projects/runcom)

---
+++ lib/sod/prefabs/actions/config/create.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require "refinements/pathnames"
+
+module Sod
+  module Prefabs
+    module Actions
+      module Config
+        # Creates project configuration.
+        class Create < Action
+          include Import[:kernel, :logger]
+
+          using Refinements::Pathnames
+
+          description "Create default configuration."
+
+          ancillary "Prompts for local or global path."
+
+          on %w[-c --create]
+
+          def initialize(xdg_config = nil, defaults_path: nil, **)
+            super(**)
+            @xdg_config = context[xdg_config, :xdg_config]
+            @defaults_path = Pathname context[defaults_path, :defaults_path]
+          end
+
+          def call(*)
+            ARGV.clear
+            check_defaults && choose
+          end
+
+          private
+
+          attr_reader :xdg_config, :defaults_path
+
+          def check_defaults
+            return true if defaults_path.exist?
+
+            logger.fatal { "Default configuration doesn't exist: #{defaults_path.to_s.inspect}." }
+            kernel.abort
+            false
+          end
+
+          def choose
+            kernel.print "Would you like to create (g)lobal, (l)ocal, or (n)o configuration? " \
+                         "(g/l/n)? "
+            response = kernel.gets.chomp
+
+            case response
+              when "g" then create xdg_config.global
+              when "l" then create xdg_config.local
+              else quit
+            end
+          end
+
+          # :reek:TooManyStatements
+          def create path
+            path_info = path.to_s.inspect
+
+            if path.exist?
+              logger.warn { "Skipped. Configuration exists: #{path_info}." }
+            else
+              defaults_path.copy path.make_ancestors
+              logger.info { "Created: #{path_info}." }
+            end
+          end
+
+          def quit
+            logger.info { "Creation canceled." }
+            kernel.exit
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/sod/prefabs/actions/config/create_spec.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+require "runcom"
+require "spec_helper"
+
+RSpec.describe Sod::Prefabs::Actions::Config::Create do
+  using Refinements::Loggers
+  using Refinements::Pathnames
+
+  subject(:action) { described_class.new xdg_config, defaults_path: }
+
+  include_context "with application dependencies"
+  include_context "with temporary directory"
+
+  let(:path) { temp_dir.join "test.yml" }
+  let(:xdg_config) { Runcom::Config.new path }
+  let(:defaults_path) { temp_dir.join("defaults.yml").write "name: test" }
+
+  describe "#call" do
+    it "aborts when defaults don't exist" do
+      defaults_path.delete
+      action.call
+
+      expect(kernel).to have_received(:abort)
+    end
+
+    it "logs fatal when defaults don't exist" do
+      defaults_path.delete
+      action.call
+
+      expect(logger.reread).to match(
+        /🔥.+Default configuration doesn't exist: #{defaults_path.to_s.inspect}.+/
+      )
+    end
+
+    shared_examples "a created file" do
+      it "creates when path doesn't exist" do
+        action.call
+        expect(path.read).to eq("name: test")
+      end
+
+      it "logs info when path doesn't exist" do
+        action.call
+        expect(logger.reread).to match(/🟢.+Created: #{path.to_s.inspect}.+/)
+      end
+
+      it "logs warning when it exists" do
+        path.touch
+        action.call
+
+        expect(logger.reread).to match(/⚠️.+Skipped. Configuration exists: #{path.to_s.inspect}.+/)
+      end
+    end
+
+    context "when global" do
+      before do
+        allow(kernel).to receive(:gets).and_return("g\n")
+        allow(xdg_config).to receive(:global).and_return path
+      end
+
+      it_behaves_like "a created file"
+    end
+
+    context "when local" do
+      before do
+        allow(kernel).to receive(:gets).and_return("l\n")
+        allow(xdg_config).to receive(:local).and_return path
+      end
+
+      it_behaves_like "a created file"
+    end
+
+    it "exits when user enters no" do
+      allow(kernel).to receive(:gets).and_return("n\n")
+      action.call
+
+      expect(kernel).to have_received(:exit)
+    end
+
+    it "exits when user enters invalid key" do
+      allow(kernel).to receive(:gets).and_return("x\n")
+      action.call
+
+      expect(kernel).to have_received(:exit)
+    end
+
+    it "logs info when exited" do
+      allow(kernel).to receive(:gets).and_return("n\n")
+      action.call
+
+      expect(logger.reread).to match(/🟢.+Creation canceled.+/)
+    end
+
+    it "fails with no path or context" do
+      expectation = proc { described_class.new }
+      expect(&expectation).to raise_error(Sod::Error, /xdg_config/)
+    end
+  end
+end
