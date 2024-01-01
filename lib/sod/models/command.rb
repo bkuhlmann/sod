19f86503938eed7cef09494f5ce0e350a807fc1a
Brooke Kuhlmann <brooke@alchemists.io>
2024-01-01 11:43:48 -0700

    Added version release notes

--- lib/sod/models/command.rb
+++ lib/sod/models/command.rb
@@ -2,6 +2,7 @@
 
 module Sod
   module Models
+    # Defines all attributes of a command.
     Command = Data.define :handle, :description, :ancillary, :actions, :operation do
       def initialize handle:, description:, actions:, operation:, ancillary: []
         super
